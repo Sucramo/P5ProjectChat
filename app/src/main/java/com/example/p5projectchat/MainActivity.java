78fbc595f7fcdc54f5b72efa234d846a41bea68d
marcu <mkol17@student.aau.dk>
2019-10-25 14:42:11 +0200

    Initial commit

---
+++ app/src/main/java/com/example/p5projectchat/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.p5projectchat;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
