c87fdec07261a48febe65e39fd199143aa4a0c6d
marcu <mkol17@student.aau.dk>
2019-11-04 20:21:49 +0100

    Firebase branch not sure I did it right

--- app/src/main/java/com/example/p5projectchat/MainActivity.java
+++ app/src/main/java/com/example/p5projectchat/MainActivity.java
@@ -3,11 +3,26 @@ package com.example.p5projectchat;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mAuth = FirebaseAuth.getInstance();
     }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // check if the user is signed in and update UI accordingly
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+    }
+
 }
