3b3b21636a7f15372278fb84851a9e6b4401d726
marcu <mkol17@student.aau.dk>
2019-11-21 13:02:58 +0100

    Firebase setup and User class implementation Done. User creation can be done via email and password

---
+++ app/src/main/java/com/example/p5projectchat/ProjectManagement/FirstPage.java
@@ -0,0 +1,14 @@
+package com.example.p5projectchat.ProjectManagement;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.example.p5projectchat.R;
+
+public class FirstPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first_page);
+    }
+}
