7c620cc46c00b77a737e62b98a87c89442dad563
martinsander <msa17@student.aau.dk>
2019-11-22 13:11:06 +0100

    Added toolbar and settings button

--- app/src/main/java/com/example/p5projectchat/ProjectManagement/FirstPageActivity.java
+++ app/src/main/java/com/example/p5projectchat/ProjectManagement/FirstPageActivity.java
@@ -1,8 +1,17 @@
 package com.example.p5projectchat.ProjectManagement;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
+
 import com.example.p5projectchat.R;
+
 import java.lang.Object;
 
 public class FirstPageActivity extends AppCompatActivity {
@@ -13,5 +22,34 @@ public class FirstPageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first_page);
+
+        //Toolbar insertion
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    //Popup menu inflater
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.popup_menu, menu);
+        return true;
+    }
+
+    //Settings button
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.item1:
+                Toast.makeText(this, "Item 1 selected", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.item2:
+                Toast.makeText(this, "Item 2 selected", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.item3:
+                Toast.makeText(this, "Item 3 selected", Toast.LENGTH_SHORT).show();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
