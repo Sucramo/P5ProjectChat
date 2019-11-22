9ad0cb9c330c7d52726a42529ca747c3806104f1
marcu <mkol17@student.aau.dk>
2019-11-22 14:00:36 +0100

    It WORKS

--- app/src/main/java/com/example/p5projectchat/UserSettings/EditUserActivity.java
+++ app/src/main/java/com/example/p5projectchat/UserSettings/EditUserActivity.java
@@ -3,14 +3,20 @@ package com.example.p5projectchat.UserSettings;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.p5projectchat.R;
 
 public class EditUserActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_user);
+
+
+
     }
 }
--- app/src/main/java/com/example/p5projectchat/UserSettings/UserSettingsActivity.java
+++ app/src/main/java/com/example/p5projectchat/UserSettings/UserSettingsActivity.java
@@ -2,12 +2,18 @@ package com.example.p5projectchat.UserSettings;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.p5projectchat.Database.User;
 import com.example.p5projectchat.R;
@@ -26,6 +32,10 @@ public class UserSettingsActivity extends AppCompatActivity {
     private FirebaseUser firebaseUser;
     private FirebaseDatabase database;
 
+    private static int RESULT_LOAD_IMAGE = 1;
+
+
+
 
 
     @Override
@@ -36,6 +46,7 @@ public class UserSettingsActivity extends AppCompatActivity {
         final TextView fname = (TextView) findViewById(R.id.get_first_name);
         final TextView lname = (TextView) findViewById(R.id.get_last_name);
         final TextView email = (TextView) findViewById(R.id.get_email);
+        ImageButton profilePic = (ImageButton) findViewById(R.id.user_image_button);
         Button editProfile = (Button) findViewById(R.id.edit_user_button);
 
         database = FirebaseDatabase.getInstance();
@@ -61,9 +72,50 @@ public class UserSettingsActivity extends AppCompatActivity {
 
     }
 
+    public void editProfilePicture(View view){
+        if (checkGalleryPermissions()){
+            Intent intent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+            startActivityForResult(intent, RESULT_LOAD_IMAGE);
+        } else {
+            Toast failureToast = Toast.makeText(this, "Something went wrong", Toast.LENGTH_SHORT);
+            failureToast.show();
+        }
+    }
+
     public void editProfileButton(View view){
         startActivity(new Intent(this, EditUserActivity.class));
         finish();
+    }
+
+    public void changeProfilePicture(View view){
+
+    }
+
+    private boolean checkGalleryPermissions(){
+        // Here, this is the current activity
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.READ_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // Permission is not granted
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.READ_EXTERNAL_STORAGE)) {
+
+                return false;
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+
+            } else {
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                        RESULT_LOAD_IMAGE);
+                return true;
+            }
+        } else {
+            // Permission has already been granted
+            return true;
+        }
     }
 
 }
